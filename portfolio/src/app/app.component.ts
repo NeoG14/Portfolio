3eb0b807b5099bdc74edcc310a02a7a299fac145
Nicolás Alvarez de Oliveira <nicolaplatajob@gmail.com>
2024-08-12 18:28:35 -0300

    Minor changes

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TypewriterComponent } from "./typewriter/typewriter.component";
+import { FormComponent } from "./form/form.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [TypewriterComponent],
+  imports: [TypewriterComponent, FormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
